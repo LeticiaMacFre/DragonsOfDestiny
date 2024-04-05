1769a0ac88241c152114921b5b2d3b186439bab9
LeticiaMacFre <leticia.macedo2705@gmail.com>
2024-04-05 14:09:32 -0300

    primeiro commit

---
+++ Assets/Scripts/robsonMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robsonMove : MonoBehaviour
+{
+
+    public float Speed;
+    public float JumpForce;
+    public bool isJumping;
+
+    private Rigidbody2D rig;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed; 
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collisor)
+    {
+        if (collisor.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+
+}
